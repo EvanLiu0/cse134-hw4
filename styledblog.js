7054830e4616f458a001668d34a1a94d8b865381
EvanLiu0 <ealiu@ucsd.edu>
2023-03-02 16:39:26 -0800

    Google Analytics

--- styledblog.js
+++ styledblog.js
@@ -1,11 +1,11 @@
-import "./node_modules/dompurify/dist/purify.js";
+// import "./node_modules/dompurify/dist/purify.js";
 import {
   showDeleteDialog,
   showEditDialog,
   showAddDialog,
-} from "./cruddialogs.js";
+} from "./styledcruddialogs.js";
 
-export var db = [];
+export var styleddb = [];
 var firstLoad = localStorage.getItem("firstLoad");
 
 export function isNew() {
@@ -18,8 +18,8 @@ export function setSeen() {
 }
 
 export function notifyDBChanged() {
-  localStorage.setItem("db", JSON.stringify(db));
-  db = JSON.parse(localStorage.getItem("db"));
+  localStorage.setItem("styleddb", JSON.stringify(styleddb));
+  styleddb = JSON.parse(localStorage.getItem("styleddb"));
   document.getElementById("list-container").innerHTML = "";
   renderExisting();
 }
@@ -32,8 +32,8 @@ function createLIFromString(htmlstring) {
 }
 
 function taggedBlog(strings, name, date, summary) {
-  return DOMPurify.sanitize(`${name} (${date}) - ${summary} 
-    <button class="edit-btn">Edit</button> <button class="delete-btn">Delete</button>`);
+  return DOMPurify.sanitize(`<h2 class="title">${name}</h2> <p class="date">(${date})</p> <p class="summary">${summary}</p> 
+    <p><i class="edit-btn">&#x270E;</i> <i class="delete-btn">&#128465;</i></p>`);
 }
 
 function coupleOnClicks() {
@@ -51,16 +51,16 @@ function coupleOnClicks() {
 }
 
 export function editBlog() {
-  showEditDialog(this.parentElement.innerHTML);
+  showEditDialog(this.parentElement.parentElement.innerHTML);
 }
 
 export function deleteBlog() {
-  showDeleteDialog(this.parentElement.innerHTML);
+  showDeleteDialog(this.parentElement.parentElement.innerHTML);
 }
 
 export function dbFind(query) {
-  for (let i = 0; i < db.length; i++) {
-    if (db[i] == query) return i;
+  for (let i = 0; i < styleddb.length; i++) {
+    if (styleddb[i] == query) return i;
   }
   return -1;
 }
@@ -70,24 +70,24 @@ export function renderExisting() {
     document.getElementById("empty-message").remove();
   }
 
-  db = JSON.parse(localStorage.getItem("db"));
-  if (db) {
-    if (db.length == 0) {
+  styleddb = JSON.parse(localStorage.getItem("styleddb"));
+  if (styleddb) {
+    if (styleddb.length == 0) {
       let emptyMessage = document.createElement("p");
       emptyMessage.setAttribute("id", "empty-message");
       emptyMessage.innerText = "No blog posts currently listed\n";
-      document.getElementById("add-btn").before(emptyMessage);
+      document.getElementById("add-btn").after(emptyMessage);
     } else {
-      for (let i = 0; i < db.length; i++) {
+      for (let i = 0; i < styleddb.length; i++) {
         document
           .getElementById("list-container")
-          .appendChild(createLIFromString(DOMPurify.sanitize(db[i])));
+          .appendChild(createLIFromString(DOMPurify.sanitize(styleddb[i])));
       }
     }
 
     coupleOnClicks();
   } else {
-    db = [];
+    styleddb = [];
     initialize();
   }
 }
@@ -100,7 +100,7 @@ export function initialize() {
     "Learn about the technical requirements and emotional damage that come with being a Machine Learning major at UCSD!";
   p1.innerHTML = taggedBlog`${p1Title}${p1Date}${p1Summary}`;
 
-  db.push(p1.innerHTML);
+  styleddb.push(p1.innerHTML);
 
   let p2 = document.createElement("li");
   let p2Title = "How Not to Learn Polymorphism";
@@ -109,7 +109,7 @@ export function initialize() {
     "Ever wondered what you should definitely not do when studying one of the three tenets of object-oriented programing? Look no further!";
   p2.innerHTML = taggedBlog`${p2Title}${p2Date}${p2Summary}`;
 
-  db.push(p2.innerHTML);
+  styleddb.push(p2.innerHTML);
 
   let p3 = document.createElement("li");
   let p3Title = "Neon Law Foundation: Cyberlaw at it's Most Human";
@@ -118,7 +118,7 @@ export function initialize() {
     "Testimonials from Las Vegas' most driven proponents of cybersecurity and digital awareness.";
   p3.innerHTML = taggedBlog`${p3Title}${p3Date}${p3Summary}`;
 
-  db.push(p3.innerHTML);
+  styleddb.push(p3.innerHTML);
 
   document.getElementById("list-container").appendChild(p1);
   document.getElementById("list-container").appendChild(p2);
